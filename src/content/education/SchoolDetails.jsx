e4fc38fbcca245286b718b8351ccf1e95946f0a0
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-07-14 16:35:22 -0700

    Add styling and responsiveness to school details component

--- src/content/education/SchoolDetails.jsx
+++ src/content/education/SchoolDetails.jsx
@@ -14,22 +14,26 @@ import CourseList from "@/content/education/CourseList";
 
 const SchoolDetails = () => {
     return (
-        <Card>
+        <Card className='bg-cyan-800'>
             <CardHeader>
                 <CardTitle className='text-center'>School Name</CardTitle>
-                <CardDescription className='grid grid-cols-2'>
-                    <p className='justify-self-start'>Degree: awarded</p>
-                    <p className='justify-self-end'>Years attended: 2000-2004</p>
-                    <p className='justify-self-start'>Standing: Graduated</p>
-                    <p className='justify-self-end'>Cumulative GPA: 3.500 out of 4.000</p>
+                <CardDescription className='grid grid-cols-1 justify-items-center md:grid-cols-2'>
+                    <p className='md:justify-self-start'>Degree: awarded</p>
+                    <p className='md:justify-self-end'>Years attended: 2000-2004</p>
+                    <p className='md:justify-self-start'>Standing: Graduated</p>
+                    <p className='md:justify-self-end'>Cumulative GPA: 3.500 out of 4.000</p>
                 </CardDescription>
                 <CardContent>
-                <Button 
-                    className='text-white hover:text-black bg-zinc-800 hover:bg-slate-200'
-                    variant='outline'
-                    asChild>
-                        <a href="https://www.example.com">Unofficial Transcript</a>
-                </Button>
+                    <div className="grid grid-cols-1 justify-items-center md:grid-cols-2">
+                        <p className='md:justify-self-start'>Courses Taken @ School Name</p>
+                        <Button 
+                            className='md:justify-self-end text-white hover:text-black bg-zinc-800 hover:bg-slate-200'
+                            variant='outline'
+                            aschild>
+                            <a href="https://www.example.com">Unofficial Transcript</a>
+                        </Button>
+                    </div>
+                
                     <CourseList />
                 </CardContent>
             </CardHeader>
