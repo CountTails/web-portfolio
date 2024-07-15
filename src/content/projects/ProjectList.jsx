a060ce6d7273f539ea91e0e6bdd31d7dcac6f4f9
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-07-14 17:26:41 -0700

    Add structure to project details components

--- src/content/projects/ProjectList.jsx
+++ src/content/projects/ProjectList.jsx
@@ -6,7 +6,7 @@ const ProjectList = () => {
     const projectArr = [1, 2, 3, 4, 5, 6];
     return (
         <section>
-            <div className='container grid grid-cols-1 md:grid-cols-2 lg:gid-cols-4 justify-items-center py-16'>
+            <div className='container grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center py-16'>
                 {projectArr.map((i) => {
                     return (
                         <ProjectDetails />
--- src/content/projects/_ProjectDetails.jsx
+++ src/content/projects/_ProjectDetails.jsx
@@ -5,22 +5,42 @@ import {
     CardContent,
     CardFooter,
     CardHeader,
+    CardDescription,
     CardTitle,
   } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const ProjectDetails = () => {
     return (
-        <Card className='bg-cyan-500 my-4'>
+        <Card className='bg-cyan-500 my-4 text-center'>
             <CardHeader>
                 <CardTitle>Project name</CardTitle>
+                <CardDescription className='grid justify-items-center grid-cols-2'>
+                    <p className='md:justify-self-start'>Role:</p>
+                    <p className='md:justify-self-end'>Contributor</p>
+                    <p className='md:justify-self-start'>Active time:</p>
+                    <p className='md:justify-self-end'>2 months</p>
+                    <p className='md:justify-self-start'>Last contributed:</p>
+                    <p className='md:justify-self-end'>January 2020</p>
+                </CardDescription>
             </CardHeader>
             <CardContent>
-                <p>Project Description</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec massa sapien.</p>
             </CardContent>
-            <CardFooter>
-                <p>Project details</p>
+            <CardFooter className='grid justify-items-center grid-cols-2'>
+                <Button 
+                    className='text-white hover:text-black bg-zinc-800 hover:bg-slate-200'
+                    variant='outline'
+                    aschild>
+                    <a href='https://www.example.com'>Repository</a>
+                </Button>
+                <Button 
+                    className='text-white hover:text-black bg-zinc-800 hover:bg-slate-200'
+                    variant='outline'
+                    aschild>
+                    <a href='https://www.example.com'>Live Demo</a>
+                </Button>
             </CardFooter>
-            
         </Card>
     )
 };
